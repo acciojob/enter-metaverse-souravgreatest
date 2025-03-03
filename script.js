c036824d32565095dc18fb36520bcd99a78964f5
sourav jain <95373772+souravgreatest@users.noreply.github.com>
2025-03-03 17:29:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let para=document.getElementById('status')
+let btn=document.getElementById('enterBtn')
+
+btn.addEventListener('click',()=>{
+	para.innerHTML='<h1>Entered Metaverse';
+});
